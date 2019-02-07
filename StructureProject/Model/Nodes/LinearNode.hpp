e0d174e2584f9128e32604aa1c85895c35418b6f
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2019-02-07 13:11:23 -0700

    Finished LinearNode and made LinkedList.

--- StructureProject/Model/Nodes/LinearNode.hpp
+++ StructureProject/Model/Nodes/LinearNode.hpp
@@ -1,5 +1,5 @@
 //
-//  LinearNode.h
+//  LinearNode.hpp
 //  StructureProject
 //
 //  Created by Runyan, Jacob on 2/5/19.
@@ -12,5 +12,56 @@
 #include "Node.hpp"
 
 template <class Type>
+class LinearNode : public Node<Type>
+{
+    //This section is available to the subclasses of LinearNode
+protected:
+    LinearNode<Type> * next;
+public:
+    //Constructors
+    LinearNode();
+    LinearNode(Type data);
+    LinearNode(Type data, LinearNode<Type> * next);
+    //Methods
+    void setNextNode(LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+};
+
+//Constructors
+
+/**
+ This contructor is used for building the raw structure before values are known.
+ */
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node<Type>()
+{
+    this-> next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+
+template<class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+
+//Accessor methods
+
+template <class Type>
+void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer)
+{
+    this->next = nextNodePointer;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNextNode()
+{
+    return next;
+}
 
 #endif /* LinearNode_hpp */
