feb5d376edc1d0a024c755d04b3fc5e1cdff0ee0
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2019-02-11 13:33:44 -0700

    Finished Queue and created stack.

---
+++ StructureProject/Model/Linear/Stack.hpp
@@ -0,0 +1,16 @@
+//
+//  Stack.hpp
+//  StructureProject
+//
+//  Created by Runyan, Jacob on 2/11/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Stack_hpp
+#define Stack_hpp
+
+#include "LinkedList.hpp"
+
+template <class Type>
+
+#endif /* Stack_hpp */
