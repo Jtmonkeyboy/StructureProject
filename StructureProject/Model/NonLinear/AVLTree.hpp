158786bd3aa5b5c30273dfef3a7bef38901ea16a
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2019-04-30 12:09:29 -0600

    insertNode

---
+++ StructureProject/Model/NonLinear/AVLTree.hpp
@@ -0,0 +1,75 @@
+//
+//  AVLTree.hpp
+//  StructureProject
+//
+//  Created by Runyan, Jacob on 4/30/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef AVLTree_hpp
+#define AVLTree_hpp
+
+#include "BinarySearchTree.hpp"
+
+template <class Type>
+class AVLTree : public Binary SearchTree<Type>
+{
+private:
+    BinaryTreeNode<Type> * leftRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightDifference(BinaryTreeNode<Type> * parent);
+    
+public:
+    AVLTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+};
+
+template <class Type>
+AVLTree<Type> :: AVLTree() : BinarySearchTree<Type>()
+{
+    this->root = nullptr;
+}
+
+template <class Type>
+void AVLTree<Type> :: insert(Type item)
+{
+    insertNode(this->getRoot(), item);
+}
+
+template <class Type>
+void AVLTree<Type> :: remove(Type item)
+{
+    
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: insertNode(BinaryTreeNode<Type> * parent, Type inserted)
+{
+    if(parent == nullptr)
+    {
+        parent = new BinaryTreeNode<Type>(inserted);
+        
+        if(this->getRoot() == nullptr)
+        {
+            this->setRoot(parent);
+        }
+        return parent;
+    }
+    else if(inserted < parent->getNodeData())
+    {
+        parent->setLeftChild(insertNode(parent->getLeftChild(), inserted));
+        parent = balancedSubTree(parent);
+    }
+    return parent;
+}
+#endif /* AVLTree_hpp */
